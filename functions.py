ffbd8eed200fbb4c6690a19af4fbe3c731f6dc93
David Lister <whizkid2012@gmail.com>
2020-12-15 12:18:01 -0600

    add

---
+++ functions.py
@@ -0,0 +1,9 @@
+
+
+def solve_using_first_buffer(matrix, mine_v1, mine_v2, mine_v3):
+    print("solving using first buffer")
+
+
+def solve_wasting_first_buffer(matrix, mine_v1, mine_v2, mine_v3):
+    print("solving wasting first buffer")
+
--- solve_v2.py
+++ solve_v2.py
@@ -1,10 +1,28 @@
 
+from functions import *
 
+#define the matrix as an ordered list (should I use dictonary?)
+matrix = [
+        '55','55','1c','1c','55',
+        '1c','55','1c','1c','55',
+        'bd','55','e9','bd','1c',
+        '55','bd','1c','55','e9',
+        'e9','1c','bd','e9','1c'
+    ]
 
-
-#define the matrix as an ordered list (should I use dictonary)
 
 #define the Datamine sequences as lists of strings
+mine_v1 = ['55','1c','e9']
+mine_v2 = ['bd','55','1c']
+mine_v3 = ['e9','e9','1c']
+
+# matrix indexes
+# '00','01','02','03','04',
+# '05','06','07','08','09',
+# '10','11','12','13','14',
+# '15','16','17','18','19',
+# '20','21','22','23','24'
+
 
 # find all matrix indices where the first string of any of the wanted sequence matches the top row
 
@@ -12,13 +30,4 @@
 
 
 # if we are not able to use the first buffer in the best way, then we will revert to wasting the first buffer in order to get the best possible result
-
-
-
-def solve_using_first_buffer(matrix, mine_v1_seq, mine_v2_seq, mine_v3_seq):
-    print("solving using first buffer")
-
-
-def solve_wasting_first_buffer(matrix, mine_v1_seq, mine_v2_seq, mine_v3_seq):
-    print("solving wasting first buffer")
 
