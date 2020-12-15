f00ccd5951aeba5b8af80bc5c66d6dac4712185b
David Lister <whizkid2012@gmail.com>
2020-12-15 12:07:00 -0600

    more work

---
+++ solve_v2.py
@@ -0,0 +1,24 @@
+
+
+
+
+#define the matrix as an ordered list (should I use dictonary)
+
+#define the Datamine sequences as lists of strings
+
+# find all matrix indices where the first string of any of the wanted sequence matches the top row
+
+# add all indices where the first requested sequence is anywhere in the matrix 
+
+
+# if we are not able to use the first buffer in the best way, then we will revert to wasting the first buffer in order to get the best possible result
+
+
+
+def solve_using_first_buffer(matrix, mine_v1_seq, mine_v2_seq, mine_v3_seq)
+
+
+
+def solve_wasting_first_buffer(matrix, mine_v1_seq, mine_v2_seq, mine_v3_seq)
+
+
